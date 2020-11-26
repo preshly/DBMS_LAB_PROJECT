e69bc84dafc3b002094b4bb385d78b156f56be5f
preshlyf1911 <mca.1911@unigoa.ac.in>
2020-11-26 11:35:20 +0530

    config

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 
 #secret for the flask app
-secret_key = 'b\x92\xf6\x8eO\x18\xf5o\xb4M\xc5^H\x0f\x85\xd2\xd5\x9d\xbe\x95\xe6\x7f\x04\xf3\xab'
+secret_key = 'youwillneverguess'
 
 #MySql server configurations on the system
 dbconfig = { 'host': '127.0.0.1',
-            'user': 'dbmslab',
-            'password': 'projectLab.1',
-            'database': 'labDB', }
+            'user': 'user',
+            'password': 'foruserpasswordfordb',
+            'database': 'useyourdb', }
